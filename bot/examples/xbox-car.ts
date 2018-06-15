e7a47694cf5cce47c299afa5cf71a0bf98b7d456
Marcin Biernat <mb@marcinbiernat.pl>
2018-06-15 17:45:10 +0200

    xbox car example

---
+++ bot/examples/xbox-car.ts
@@ -0,0 +1,34 @@
+import * as R from 'ramda';
+
+import '../bot';
+import { buildXpadStream, toXboxState, applyDeadzone, XPadState } from 'src/xpad';
+import { towerPro, dc50hz } from 'src/peripheries';
+import { createServo, setAngle } from 'src/servo';
+import { createABController } from 'src/dc';
+import { setMultiAxesDCPower } from 'src/controls';
+
+
+const steeringServo = createServo(towerPro, 18);
+
+
+const mainMotor = createABController(dc50hz, {a: 13, b: 6, pwm: 5})
+
+
+const steer = (state: XPadState) => {
+  setAngle(steeringServo, state.axes['0'] * 90);
+  setMultiAxesDCPower(mainMotor, state.axes["5"], state.axes["2"]);
+};
+
+
+const run = async () => {
+  buildXpadStream(
+    R.pipe(
+      toXboxState,
+      applyDeadzone(0.15),
+      steer,
+    ),
+  );
+}
+
+
+run().catch(console.error);
